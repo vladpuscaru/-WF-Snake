3d3a7ff711a5e0c768d50ac9fa296ec3797d8336
Vlad-Cristian Puscaru <vlad.cpuscaru@gmail.com>
2019-03-23 00:22:58 +0200

    Add project files.

---
+++ WF_Snake_V3/Fruit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WF_Snake_V3
+{
+	class Fruit : Entity
+	{
+		public int Value { get; set; }
+
+		public Fruit(int maxWidth, int maxHeight, int boxSize)
+		{
+			Value = (new Random()).Next(1, 4); // creates a random value between 1 and 3
+			X = ((new Random()).Next(0, maxWidth / boxSize)) * boxSize;
+			Y = ((new Random()).Next(0, maxHeight / boxSize)) * boxSize;
+			Width = boxSize;
+			Height = boxSize;
+
+			switch (Value)
+			{
+				case 1: Color = System.Drawing.Brushes.Blue; break;
+				case 2: Color = System.Drawing.Brushes.Beige; break;
+				case 3: Color = System.Drawing.Brushes.CadetBlue; break;
+			}
+		}
+	}
+}
